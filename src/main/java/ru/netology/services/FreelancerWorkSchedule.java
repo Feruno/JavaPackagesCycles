cea718250b19a34b34775f7f702f5becaa8fa09f
Александр Бондаренко <nice.alexsandr@mail.ru>
2023-05-09 20:00:14 +0300

    initial commit

---
+++ src/main/java/ru/netology/services/FreelancerWorkSchedule.java
@@ -0,0 +1,18 @@
+package ru.netology.services;
+
+public class FreelancerWorkSchedule {
+    public int calculate(int threshold, int income, int expenses) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = (threshold + expenses) / 3;
+            } else {
+                money += income + expenses;
+            }
+        }
+        return count;
+    }
+}
